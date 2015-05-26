18043619b0b44f9067071c66d9287b4f4cbc0afe
wjlevay <wjlevay@gmail.com>
2015-05-25 23:04:56 -0400

    Fixed footer

--- footer.php
+++ footer.php
@@ -14,9 +14,9 @@
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
 			<img class="footer-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/circle-logo.png" />
-			<p>&copy;2006&ndash;<?php echo date('Y'); ?> Bandulos Entertainment</p>
-			<address>99 Moffett Street<br />
-			Staten Island, NY 10312</address>
+			<p><?php echo date('Y'); ?> Bandulos, LLC</p>
+			<address>22 Crescent Ave<br />
+			Staten Island, NY 10301</address>
 			<p><a href="tel:+13479349607">347.934.9607</a><br />
 			<a href="mailto:info@bandulos.com">info@bandulos.com</a></p>
 		</div><!-- .site-info -->
